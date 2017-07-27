ab5617249387314a6a7c54a170878f790b3d98ec
urbaniak <urbaniaknicolas62@gmail.com>
2017-07-27 14:10:14 +0200

    POO comparaison

--- js/main.js
+++ js/main.js
@@ -20,35 +20,66 @@ function user (prenom ,nom ,age ,pays ,moyenDePaiement ){
   this.age=age,
   this.pays=pays,
   this.moyenDePaiment=moyenDePaiement,
-  this.getName = function() {
-   alert(this.prenom + this.nom + this.age + this.pays + this.moyenDePaiment);
+  // this.getName = function() {
+   alert(" prenom : " + this.prenom + " nom : " + this.nom + " age : " + this.age + " pays : "
+    + this.pays + " methode de paiement :" + this.moyenDePaiment);
 
- };
+ // };
  this.année=function(){
    alert(2017-this.age);
  }
 }
-var user1=new user("prenom : nicolas / ","nom : urbaniak / age : ", 38   ," /  pays : france / ",
-" moyen de paiement : ( cb , master card , bitcoins .)");
-user1.getName();
+var user1=new user(" nicolas . "," urbaniak . ", 38  ," FRANCE .",
+" ( cb , master card , bitcoins .)");
+// user1.getName();
 user1.année();
+
+/*------------------METHODE COMPARAISON----------------------------*/
+
+function comparaison (pays ,age ,moyenDePaiement){
+  this.pays=pays,
+  this.age=age,
+  this.moyenDePaiement=moyenDePaiement,
+  this.compare= function(){
+
+  };
+
+};
+  var comparaitre=new comparaison(" FRANCE .",18,"cb");
+
+  /*---------------AGE-DU-CLIENT----------------*/
+if(comparaitre.age<user1.age){
+  alert("c'est GOOD..");
+}
+  else{
+  alert("vous etes trop jeune.");
+  /*---------------PAYS--------------------------*/
+};
+if(comparaitre.pays!=user1.pays){
+  alert("vous n'etes pas au bon endroit")
+}
+else {
+  alert("bienvenue");
+};
+/*--------------------METHODE-DE-PAIEMENT----------------*/
+
 /*----------------------USER-2-----------------------------*/
 
-var user2=new user("Prenom : rocco / "," nom : sifredi / age : ", 45 ,"ans / pays : italie / "," moyen de Paiement : NATURE .");
-user2.getName();
+// var user2=new user("Prenom : rocco / "," nom : sifredi / age : ", 45 ,"ans / pays : italie / "," moyen de Paiement : NATURE .");
+// user2.getName();
 
 /*-------------------ESSAI 1---------------------------*/
 
-function jean (taille,longueur,couleur,){
-  this.taille=taille,
-  this.longueur=longueur,
-  this.couleur=couleur,
-  this.getJean=function(){
-  alert(this.taille + this.longueur + this.couleur);
-}
-};
-var client= new jean( "32 " , " 178 cm " , " bleu .");
-client.getJean();
-
-var client2= new jean("taille : 32 / " , " hauteur : 195 cm / " , "couleur : beige");
-client2.getJean();
+// function jean (taille,longueur,couleur,){
+//   this.taille=taille,
+//   this.longueur=longueur,
+//   this.couleur=couleur,
+//   this.getJean=function(){
+//   alert(this.taille + this.longueur + this.couleur);
+// }
+// };
+// var client= new jean( "tour de taille : 32 / " , "hauteur :  178 cm / " , "couleur : bleu .");
+// client.getJean();
+//
+// var client2= new jean("taille : 32 / " , " hauteur : 195 cm / " , "couleur : beige");
+// client2.getJean();
